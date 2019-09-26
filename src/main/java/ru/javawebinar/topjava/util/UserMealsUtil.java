53aa53939be5fd47b8460787017b0469b2bcd8f1
Andrey Gudilov <gudilov@netcracker.com>
2019-09-25 22:19:10 -0600

    Fix 'exceed' calculation logic

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,15 +3,15 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class UserMealsUtil {
+
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
@@ -21,10 +21,10 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        System.out.println(getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 700/*2000*/));
+        System.out.println(getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
 //        .toLocalDate();
 //        .toLocalTime();
-        System.out.println(getFilteredWithExceededOptional1(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 700/*2000*/));
+        System.out.println(getFilteredWithExceededOptional1(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
 
@@ -32,25 +32,44 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
 
+        Map<LocalDate, Integer> calSumMap = new HashMap<>();
+
+        for (UserMeal meal : mealList) {
+            LocalDate date = meal.getDateTime().toLocalDate();
+            calSumMap.merge(date, meal.getCalories(), Integer::sum);
+        }
+
         List<UserMealWithExceed> result = new ArrayList<>();
 
         for (UserMeal meal : mealList) {
-            LocalTime mealTime = meal.getDateTime().toLocalTime();
-            if (TimeUtil.isBetween(mealTime, startTime, endTime)) {
-                int mealCalories = meal.getCalories();
-                result.add(new UserMealWithExceed(meal, mealCalories > caloriesPerDay));
+            LocalDateTime dateTime = meal.getDateTime();
+
+            if (TimeUtil.isBetween(dateTime.toLocalTime(), startTime, endTime)) {
+                LocalDate date = dateTime.toLocalDate();
+                result.add(new UserMealWithExceed(dateTime, meal.getDescription(), meal.getCalories(), calSumMap.get(date) > caloriesPerDay));
             }
         }
+
         return result;
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceededOptional1(List<UserMeal> mealList, LocalTime
             startTime, LocalTime endTime, int caloriesPerDay) {
 
-        return mealList
-                .stream()
-                .filter(mealitem -> TimeUtil.isBetween(mealitem.getDateTime().toLocalTime(), startTime, endTime))
-                .map(meal -> new UserMealWithExceed(meal, meal.getCalories() > caloriesPerDay)).collect(Collectors.toList());
+        Map<LocalDate, Integer> calSumMap = new HashMap<>();
+
+        mealList.forEach(meal -> {
+            LocalDate date = meal.getDateTime().toLocalDate();
+            calSumMap.merge(date, meal.getCalories(), Integer::sum);
+        });
+
+        return mealList.stream()
+                .filter(item -> TimeUtil.isBetween(item.getDateTime().toLocalTime(), startTime, endTime))
+                .map(meal -> {
+                    LocalDate date = meal.getDateTime().toLocalDate();
+                    return new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), calSumMap.get(date) > caloriesPerDay);
+                }).collect(Collectors.toList());
 
     }
+
 }
