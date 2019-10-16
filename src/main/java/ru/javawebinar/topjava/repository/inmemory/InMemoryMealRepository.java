25abf44d1b1e427715d0dd8eb7e7437e5adddb0a
Andrey Gudilov <gudilov@netcracker.com>
2019-10-16 04:03:31 -0600

    Implement repository through nested maps - Map<userId,Map<MealId,Meal>>

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -17,79 +17,80 @@ import java.util.stream.Collectors;
 @Repository
 public class InMemoryMealRepository implements MealRepository {
     private final Logger log = LoggerFactory.getLogger(getClass());
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
-    private Map<Integer, Set<Integer>> userIndex = new ConcurrentHashMap<>();
+    private Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
     @Override
-    synchronized public Meal save(Meal meal) {
+    public Meal save(Meal meal) {
 
-        Meal result = null;
+        int userId = meal.getUserId();
+
+        Map<Integer, Meal> userRepository = repository.get(userId);
+
+        if (userRepository == null) {
+            userRepository = new ConcurrentHashMap<>();
+            repository.put(userId, userRepository);
+        }
 
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
-            result = meal;
+            userRepository.put(meal.getId(), meal);
+            return meal;
         } else {
-            result = repository.computeIfPresent(meal.getId(),
+            Meal result = userRepository.computeIfPresent(meal.getId(),
                     (id, oldMeal) -> Objects.equals(oldMeal.getUserId(), meal.getUserId()) ? meal : oldMeal);
             //cannot return null in the lambda as null in the lambda will remove the key
+            return Objects.equals(result, meal) ? meal : null;
         }
-
-        //this Objects.equals check is needed because, if return null is inside computeIfPresent - the key is removed
-        if (Objects.equals(result, meal)) {
-            Set<Integer> mealSet = userIndex.get(meal.getUserId());
-            if (mealSet == null) {
-                mealSet = new CopyOnWriteArraySet<>();
-                userIndex.put(meal.getUserId(), mealSet);
-            }
-            mealSet.add(meal.getId());
-        }
-
-        return result;
     }
 
     @Override
-    synchronized public boolean delete(int id, int userID) {
+    public boolean delete(int id, int userID) {
+        Map<Integer, Meal> userRepository = repository.get(userID);
+
+        if (userRepository == null) {
+            return false;
+        }
+
         Meal meal = get(id, userID);
 
         if (meal == null) {
             return false;
         }
 
-        if (repository.remove(id) != null) {
-
-            Set<Integer> mealSet = userIndex.get(meal.getUserId());
-
-            return mealSet.remove(meal.getId());
-        }
-        return false;
+        return userRepository.remove(id) != null;
     }
 
     @Override
     public Meal get(int id, int userID) {
         log.debug("Get id {}, userID {}", id, userID);
-        Meal meal = repository.get(id);
+
+        Map<Integer, Meal> userRepository = repository.get(userID);
+        if (userRepository == null) {
+            return null;
+        }
+
+        Meal meal = userRepository.get(id);
 
         if (meal == null || meal.getUserId() != userID) {
             return null;
         }
+
         return meal;
     }
 
     @Override
     public Collection<Meal> getAllByUserId(int userID) {
         log.debug("getAll id {} items {}", userID, repository.values());
-        return userIndex.get(userID).stream()
-                .map(mealId -> repository.get(mealId))
+        return (repository.get(userID) == null) ? new ArrayList<>() : repository.get(userID).values();
+//                .stream()
 //                .sorted((meal1, meal2) -> meal2.getDateTime().compareTo(meal1.getDateTime())) //move to controller
-                .collect(Collectors.toList());
+//                .collect(Collectors.toList());
     }
 
     @Override
     public Collection<Meal> getAllByDate(int userId, LocalDate startDate, LocalDate endDate) {
-        return userIndex.get(userId).stream()
-                .map(mealId -> repository.get(mealId))
+        return getAllByUserId(userId).stream()
                 .filter(meal -> DateTimeUtil.isBetween(meal.getDate(), startDate, endDate))
 //                .sorted((meal1, meal2) -> meal2.getDateTime().compareTo(meal1.getDateTime())) //moved to Contrloller level
                 .collect(Collectors.toList());
