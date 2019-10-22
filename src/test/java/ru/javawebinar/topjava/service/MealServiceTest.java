32ef9f3669ff3d17d84fb6aaa646c54ce651904d
Andrey Gudilov <gudilov@netcracker.com>
2019-10-22 00:49:03 -0600

    fix failed tests

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -24,7 +24,8 @@ import static ru.javawebinar.topjava.UserTestData.*;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
+        "classpath:spring/spring-db.xml",
+        "classpath:spring/spring-rep.xml"
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/MealTestData.sql", config = @SqlConfig(encoding = "UTF-8"))
