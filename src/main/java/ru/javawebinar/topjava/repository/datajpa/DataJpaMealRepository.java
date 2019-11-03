c37c74f91bdb7fae2c27afc55684d08b8e303742
Andrey Gudilov <gudilov@netcracker.com>
2019-11-03 01:58:40 -0700

    TJ-4 Implement DataJpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,41 +1,63 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+import static ru.javawebinar.topjava.util.DateTimeUtil.getEndExclusive;
+import static ru.javawebinar.topjava.util.DateTimeUtil.getStartInclusive;
+
+
+@Transactional(readOnly = true)
 @Repository
 public class DataJpaMealRepository implements MealRepository {
 
-    @Autowired
-    private CrudMealRepository crudRepository;
+    private final CrudMealRepository crudRepository;
 
-    @Override
-    public Meal save(Meal meal, int userId) {
-        return null;
+    private final CrudUserRepository crudUserRepository;
+
+    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository crudUserRepository) {
+        this.crudRepository = crudRepository;
+        this.crudUserRepository = crudUserRepository;
     }
 
+    @Transactional
+    @Modifying
+    @Override
+    public Meal save(Meal meal, int userId) {
+        if (!meal.isNew() && (crudRepository.get(meal.getId(), userId) == null)) {
+            return null;
+        }
+        User user = crudUserRepository.getOne(userId); //proxy object is always returned even if the user is not in the DB
+        meal.setUser(user);
+        return crudRepository.save(meal);
+    }
+
+    @Transactional
+    @Modifying
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id, userId) > 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.get(id, userId);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.getAll(userId);
     }
 
     @Override
     public List<Meal> getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
-        return null;
+        return crudRepository.getBetweenInclusive(userId, getStartInclusive(startDate), getEndExclusive(endDate));
     }
 }
