6c7d1d3e295a03320f0eaf5f67304294e537aa46
Andrey Gudilov <gudilov@netcracker.com>
2019-11-04 13:49:52 -0700

    TJ-3 Build and test service methods

--- src/test/java/ru/javawebinar/topjava/service/MealServiceDataJPATest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceDataJPATest.java
@@ -1,14 +1,21 @@
 package ru.javawebinar.topjava.service;
 
+import org.hibernate.Hibernate;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
+import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -18,4 +25,14 @@ import ru.javawebinar.topjava.Profiles;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(Profiles.DATAJPA)
 public class MealServiceDataJPATest extends MealServiceTest{
+
+    @Transactional
+    @Test
+    public void getWithUser(){
+        Meal meal = service.get(MealTestData.MEAL1_ID, UserTestData.USER_ID);
+        User user = meal.getUser();
+
+        MealTestData.assertMatch(meal,MealTestData.MEAL1);
+        UserTestData.assertMatch(user,UserTestData.USER);
+    }
 }
