74d365ed61e48d9971bdfd4b2a700a1bef523ea0
Andrey Gudilov <gudilov@netcracker.com>
2019-11-03 21:54:23 -0700

    TJ-1 Split JdbcMealRepository for HSQLDB/Postgres using @Profile

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -12,14 +12,16 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static ru.javawebinar.topjava.util.DateTimeUtil.getEndExclusive;
 import static ru.javawebinar.topjava.util.DateTimeUtil.getStartInclusive;
 
-@Repository
-public class JdbcMealRepository implements MealRepository {
+
+public abstract class JdbcMealRepository implements MealRepository {
 
     private static final RowMapper<Meal> ROW_MAPPER = BeanPropertyRowMapper.newInstance(Meal.class);
 
@@ -45,7 +47,7 @@ public class JdbcMealRepository implements MealRepository {
                 .addValue("id", meal.getId())
                 .addValue("description", meal.getDescription())
                 .addValue("calories", meal.getCalories())
-                .addValue("date_time", meal.getDateTime())
+                .addValue("date_time", specificDateTimeFormat(meal.getDateTime()))
                 .addValue("user_id", userId);
 
         if (meal.isNew()) {
@@ -85,6 +87,8 @@ public class JdbcMealRepository implements MealRepository {
     public List<Meal> getBetweenInclusive(LocalDate startDate, LocalDate endDate, int userId) {
         return jdbcTemplate.query(
                 "SELECT * FROM meals WHERE user_id=? AND date_time >=? AND date_time < ? ORDER BY date_time DESC",
-                ROW_MAPPER, userId, getStartInclusive(startDate), getEndExclusive(endDate));
+                ROW_MAPPER, userId, specificDateTimeFormat(getStartInclusive(startDate)), specificDateTimeFormat(getEndExclusive(endDate)));
     }
+
+    protected abstract <T extends Object> T specificDateTimeFormat(LocalDateTime dateTime);
 }
---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryHsqldbImpl.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Repository
+@Profile(Profiles.HSQL_DB)
+public class JdbcMealRepositoryHsqldbImpl extends JdbcMealRepository {
+
+    public JdbcMealRepositoryHsqldbImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected <T extends Object> T specificDateTimeFormat(LocalDateTime dateTime) {
+        return (T) Timestamp.valueOf(dateTime);
+    }
+}
