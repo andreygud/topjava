6a0bd83eeafc65421f95d92703fa5ceaa20b7aa0
Andrey Gudilov <gudilov@netcracker.com>
2019-11-13 19:01:11 -0800

    TJ-14 Fixup JDBC Test

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractCacheableUserServiceTest.java
@@ -0,0 +1,19 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import ru.javawebinar.topjava.repository.JpaUtil;
+
+public class AbstractCacheableUserServiceTest extends AbstractUserServiceTest {
+    @Autowired
+    protected JpaUtil jpaUtil;
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Before
+    public void setUp() throws Exception {
+        cacheManager.getCache("users").clear();
+        jpaUtil.clear2ndLevelHibernateCache();
+    }
+}
