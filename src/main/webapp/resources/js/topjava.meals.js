9fb0ac9af990b9354dadcbd57e07b805b761108f
Andrey Gudilov <gudilov@netcracker.com>
2019-12-11 10:46:01 -0700

    TJ-52 Fix comments#3. Move converters and date pickers to meals.js

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -12,6 +12,20 @@ function clearFilter() {
 }
 
 $(function () {
+    $.ajaxSetup({
+        converters: {
+            "text json": function (stringData) {
+                var json = JSON.parse(stringData);
+                $(json).each(function () {
+                    if (this.dateTime) {
+                        this.dateTime = this.dateTime.replace('T', ' ').substr(0, 16);
+                    }
+                });
+                return json;
+            }
+        }
+    });
+
     makeEditable({
         ajaxUrl: "ajax/profile/meals/",
         datatableApi: $("#datatable").DataTable({
@@ -58,4 +72,76 @@ $(function () {
         }),
         updateTable: updateFilteredTable
     });
+
+    initializeMealsDatePickers()
 });
+
+function initializeMealsDatePickers() {
+    $("#startDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        onShow: function () {
+            this.setOptions({
+                maxDate: $('#endDate').val() ? $('#endDate').val() : false,
+                minDate: false
+            })
+        },
+        onSelectDate: function () {
+            if ($('#startDate').val() === $('#endDate').val()) {
+                $("#endTime").datetimepicker('reset');
+            }
+        }
+    });
+
+    $("#endDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        onShow: function () {
+            this.setOptions({
+                minDate: $('#startDate').val() ? $('#startDate').val() : false,
+                maxDate: false
+            })
+        },
+        onSelectDate: function () {
+            if ($('#startDate').val() === $('#endDate').val()) {
+                $("#endTime").datetimepicker('reset');
+            }
+        }
+    });
+
+    $("#startTime").datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+        onShow: function () {
+            if ($('#startDate').val() === $('#endDate').val()) {
+                this.setOptions({
+                    maxTime: $('#endTime').val() ? $('#endTime').val() : false
+                })
+            } else {
+                this.setOptions({
+                    maxTime: false
+                })
+            }
+        }
+    });
+
+    $("#endTime").datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+        onShow: function () {
+            if ($('#startDate').val() === $('#endDate').val()) {
+                this.setOptions({
+                    minTime: $('#startTime').val() ? $('#startTime').val() : false
+                })
+            } else {
+                this.setOptions({
+                    minTime: false
+                })
+            }
+        }
+    });
+
+    $(".datetime-input").datetimepicker({
+        format: 'Y-m-d H:i'
+    });
+}
